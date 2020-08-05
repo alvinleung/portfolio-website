3a213b751d69ba1101d01e2765f6c4e07f3280c5
Alvin <alvinleung2009@gmail.com>
2020-08-04 17:14:18 -0700

    Basic website setup

---
+++ dist/index.js
@@ -0,0 +1,31 @@
+window.addEventListener("load", (e) => {
+  setupRouting();
+});
+
+function setupRouting() {
+  Router.setup();
+  const projecPage = Router.useRoute("project", function () {});
+}
+
+// prevent browser refresh url changes
+//https://medium.com/js-dojo/how-to-prevent-browser-refresh-url-changes-or-route-navigation-in-vue-132e3f9f96cc
+const route = {};
+const Router = {
+  setup: () => {
+    // prevent the browser refreshing
+    const captureUnload = (e) => {
+      preventDefault();
+      // Chrome requires returnValue to be set.
+      e.returnValue = "";
+    };
+    // init the router here
+    window.addEventListener("beforeunload", captureUnload);
+
+    return () => {
+      // cleanup function
+      // init the router here
+      window.removeEventLister("beforeunload", captureUnload);
+    };
+  },
+  useRoute: (routeName, callback) => {},
+};
